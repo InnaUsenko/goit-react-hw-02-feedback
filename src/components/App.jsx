aa49a056d11dc86c8993b0cb502a210ed4f9f465
InnaUsenko <innausenko8@gmail.com>
2023-09-28 21:30:05 +0300

    add state and onClick event

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,43 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import Feedback from './Feedback/Feedback';
+const { Component } = require('react');
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback = () => {};
+  countPositiveFeedbackPercentage = () => {};
+
+  vote = event => {
+    const id = event.target.id;
+    switch (id) {
+      case 'good':
+        this.setState({ good: this.state.good + 1 });
+        break;
+      case 'neutral':
+        this.setState({ neutral: this.state.neutral + 1 });
+        break;
+      case 'bad':
+        this.setState({ bad: this.state.bad + 1 });
+        break;
+      default:
+        console.log(id + ' param is not in the state');
+    }
+  };
+  render() {
+    return (
+      <Feedback
+        good={this.state.good}
+        neutral={this.state.neutral}
+        bad={this.state.bad}
+        vote={this.vote}
+      />
+    );
+  }
+}
+
+export default App;
